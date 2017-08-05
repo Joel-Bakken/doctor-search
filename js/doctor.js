8d0bf9f6e5758a2a24f6db63fa70ea4f58c11153
Joel Bakken <jkbetc@gmail.com>
2017-08-05 11:26:44 -0700

    added two more items to Symptom constructor

--- js/doctor.js
+++ js/doctor.js
@@ -5,7 +5,7 @@ function Symptom() {
 
 }
 
-Symptom.prototype.getDoctor = function(sx, latitude, longitude, distance) {
+Symptom.prototype.getDoctor = function(sx, latitude, longitude, distance, latitude, longitude) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + sx + '&location=' + latitude + '%2C' + longitude + '$2C' + distance + '&user_location=' + latitude + '%2C' + longitude + '&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
     displayDoctor(sx, response.symptom.doctor_name);
   }).fail(function(error) {
